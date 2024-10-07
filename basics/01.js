1e360cfb846078c0bc13536fdd1ea3754d734ae2
rupendra6 <108588938+rupendra6@users.noreply.github.com>
2024-10-07 18:36:02 +0000

    data types

---
+++ basics/01.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js as newer version of js
+const accountID = 23;
+console.log(accountID);
+let email = "rupendra0060@gmail.com";
+console.log(email);
+var name = "Rupendra Singh Rajput";
+console.log(name);
+let acid = 12;
+console.table([accountID,name,email,acid])
+
