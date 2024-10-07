db2e5589209a466640a87a3746dfbd0d3e71470b
rupendra6 <108588938+rupendra6@users.noreply.github.com>
2024-10-07 18:57:58 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,16 @@
+// score = "34a";
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(score);
+// let valueNumber = Number(score);
+// console.log(valueNumber);
+
+// "22" -> 22, "22abc" -> NaN(Not a Number) and its type is number
+
+let num = 1;
+console.log(Boolean(num));
+
+// 1 -> true 0 -> false
+// "" -> false
+// "hitesh" -> true
+
