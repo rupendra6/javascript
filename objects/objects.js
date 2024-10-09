795e57fcb2afb98c77a4cf18f173925276597351
rupendra6 <108588938+rupendra6@users.noreply.github.com>
2024-10-09 11:08:25 +0000

    objects

---
+++ objects/objects.js
@@ -0,0 +1,14 @@
+//Singleton
+//Object literals
+// constructor
+
+// object literals
+
+const mySym = Symbol("Key1");
+const obj = {
+    name : "Rupendra",
+    work : "bonsai"
+}
+obj[mySym] 
+console.log(obj.work)
+console.log(obj)
